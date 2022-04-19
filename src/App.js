5daceb783193cc32241e3a7cd3a7bfd9ad3a76fc
alilatukau03 <alilatukau07@gmail.com>
2022-04-19 13:20:24 +0700

    linktr ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import ParticlesBg from "particles-bg";
+import "./App.css";
+import { BsInstagram } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card">
+        <div className="card_body">
+          <div className="profile text-center">
+            <img src="/ozzy.jpg" className="avatar" />
+            <div className="bg_content rd_12 p_8">
+              <h1>OZZY JEENIOS</h1>
+              <p>@ozzy.jeenios</p>
+              <p>Tetetp semangat ya gaes!</p>
+            </div>
+            {/* <p>Tetep semangat ya gaes!</p> */}
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://www.instagram.com/ozzy.jeenios/"
+            >
+              <BsInstagram />
+              <span>Instagram</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://www.github.com/alilatukau03/"
+            >
+              <BsGithub />
+              <span>Github</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <ParticlesBg type="color" bg={true} />
     </div>
   );
 }
